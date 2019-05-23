c887fdf37583cbd6ca5049bb720365c0a0036cc6
avsoriano <vidal_alex_69@hotmail.com>
2019-05-23 18:12:00 +0200

    version 0.1

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -9,9 +9,9 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
-    TextView mTextView;
-    Button mTopButton;
-    Button mBottomButton;
+    private TextView mTextView;
+    private Button mTopButton;
+    private Button mBottomButton;
 
     final String top = "TOP";
     final String bottom = "BOTTOM";
@@ -22,14 +22,13 @@ public class MainActivity extends AppCompatActivity {
     };
 
     private Question[] botonTop = new Question[]{
-            new Question(R.string.T1_Ans1_ES)
+            //new Question(R.string.T1_Ans1_ES)
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
         mTextView = findViewById(R.id.storyTextView);
@@ -41,25 +40,28 @@ public class MainActivity extends AppCompatActivity {
         mTopButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                updateQuestion(top);
+                mTextView.setText(R.string.T3_Story_ES);
+                mTopButton.setText(R.string.T3_Ans1_ES);
+                mBottomButton.setText(R.string.T3_Ans2_ES);
             }
         });
-
-
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
         mBottomButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                updateQuestion(bottom);
+                //updateQuestion(bottom);
+                mTextView.setText(R.string.T2_Story_ES);
+                mTopButton.setText(R.string.T2_Ans1_ES);
+                mBottomButton.setText(R.string.T2_Ans2_ES);
             }
         });
 
     }
 
     private void updateQuestion(String question){
-        if(question == top)
-            mTextView.setText();
+        //if(question == top)
+            //mTextView.setText();
     }
 
 }
